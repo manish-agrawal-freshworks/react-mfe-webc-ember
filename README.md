f77d96acce8d383f265d191078c2aed67cb51d1c
Manish Agrawal <manish.agrawal@freshworks.com>
2024-06-04 12:21:08 +0530

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # react-mfe-webc-ember
 A micro-frontend based embedding React MFE into Ember as Web component
+
+# Example: Ember host app with react mfe
+
+## Usage
+
+## Install packages and start host app
+
+```
+cd host-app
+npm install
+npm start
+
+```
+
+## Install packages and start mfe app
+
+```
+cd react-mfe
+pnpm install
+pnpm build:preview
+```
---
+++ react-mfe/src/router.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import Home from './pages/home';
+import Layout from './pages/layout';
+
+export const generateRouter = () => {
+  return createBrowserRouter([
+    {
+      path: '/react-route',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+      ],
+    },
+    {
+      path: '/taas',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+};
